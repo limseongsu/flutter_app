49eeb49cb8bc8fec80537ddabd203acb8eb8b3d7
Lss <tjdtn2323@gmail.com>
2021-03-22 10:24:33 +0900

    'first'

---
+++ lib/3_22.dart
@@ -0,0 +1,45 @@
+// main 메서드 또는 함수라고 부름
+void main() {
+  // 잘못된 이름
+
+  int a = 10;
+  int b = 20;
+
+
+  // 잘 된 이름 (camel case(낙타형)
+  int myAge = 10; //나이
+  int brotherAge = 20; //  형 나이
+
+
+  // nake case (뱀형)
+  int my_age = 10;
+  int brother_age = 20; // 형 나이
+
+
+  // 어
+  // 숫자 (int-정수,double-소숫점)
+   int d = 5.5;
+// [문자열]
+string name = '홍길동';
+var name2 = '한석봉';
+
+// [진위 (참, 거짓)]
+bool isMarried - true; //결혼했다
+  isMarried = false;  //결혼 안 했다
+
+  // 숫자 목록
+  List<int> ageList = [10, 20, 30];
+  var ageList2 = [10, 20, 30];
+  // 문자열 목록
+  List<string> nameList = ['홍길동', '한석봉'];
+var nameList2 - ['홍길동', '한석봉'];
+
+// null (값이 없다)
+int g;    //null
+int 10; //null이 아니고 10
+
+  string name3 ='';  //아무것도 없는 0개의 문자
+  string name4;     // null
+
+}
+
