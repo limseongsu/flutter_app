4c00a3a9c4234222f392ac715ed4f9a83fa208a7
Lss <tjdtn2323@gmail.com>
2021-03-22 16:03:27 +0900

    '조건,반복문,연습'

--- lib/3_22.dart
+++ lib/3_22.dart
@@ -1,3 +1,5 @@
+
+
 // main 메서드 또는 함수라고 부름
 void main() {
   // 잘못된 이름
@@ -33,26 +35,55 @@ void main() {
 
 // null (값이 없다)
 //   int g; //null
-//   int g = 10; //null이 아니고 10
+//   int g = 10; //null 이 아니고 10
 
   String name3 = ''; //아무것도 없는 0개의 문자
   String name4; // null
 
- final double tax = 1.1;   //세금   // final을 붙히면 변경을 불가하게됨 = 상수
-  int fax = 5;    //팩스 가격 (5.5)
+  final double tax = 1.1; //세금   // final을 붙히면 변경을 불가하게됨 = 상수
+  int fax = 5; //팩스 가격 (5.5)
   print('5만원짜리를 4만원으로 할인합니다.');
   fax = 4;
   print('팩스의 새로운 가격은(세금포함) ${fax * tax}만원!');
-    //4.4
-second();
-}
+  //4.4
 
-void second() {
   // main을 분리하여 사용할 수 있다.
 
-}
-
-
-void three() {
   //https://yujin-dev.tistory.com/53
+
+  var names = [
+    '한석봉',
+    '심사임당',
+    '이순신',
+  ];
+
+  //반복문 for 문 : 목록이 있으면
+  for (var name in names) {
+    print('내 이름은 $name입니다');
+  }
+
+  // 1~12까지 출력
+  // ++ : 1증가
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  // 100번 실행
+  for (int i = 0; i < 100; i++) {
+    print('내 이름은 오준석');
+  }
+
+  //조건문
+  int y = 6;
+  // == : 같은지 비교
+  // >, <, >=, <=
+  if (y > 5) {
+    print('5보다 크다');
+  } else if (y > 3) {
+    print('3보다 크다');
+  } else {
+    print('3보다 작다');
+  }
+  // shift + F6를 누르면 같은글자 ex)a가 6개있으면(같은 void안) 다 같이 편집이
+  // 가능하게 바뀐다.
 }
