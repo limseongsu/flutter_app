67e644a648fb660dd62bdf8e935189650b707337
Lss <tjdtn2323@gmail.com>
2021-03-23 15:41:42 +0900

    6부터 10까지의 주사위

--- lib/3_23.dart
+++ lib/3_23.dart
@@ -5,7 +5,29 @@ void main() {
 }
 
 int dice() {
-  // 1~6
+  // 1~10 주사위
   var random = new Random();
-  return random.nextInt(6) + 1; //print(random.nextInt(6) + 1);를 삭제해줌
+  return random.nextInt(10) + 1; //print(random.nextInt(6) + 1);를 삭제해줌
+
 }
+int dice2() {
+  var random = new Random();
+  return random.nextInt(10) + 1;  //1~10주사위
+}
+int dice3() {
+  var random = new Random();
+  return random.nextInt(9) + 1; //1~9주사위
+}
+int dice4() {
+  var random = new Random();
+  return random.nextInt(8) + 1; //1~8주사위
+}
+int dice5() {
+  var random = new Random();
+  return random.nextInt(7) + 1; //1~7주사위
+}
+int dice6() {
+  var random = new Random();
+  return random.nextInt(6) + 1; //1~6주사위
+}
+
