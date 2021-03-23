0aec98b11462f98ac167df448b7f9e4ba2401f74
Lss <tjdtn2323@gmail.com>
2021-03-23 15:52:57 +0900

    1 ~ n 까지 출력하는 주사위로 수정

--- lib/3_23.dart
+++ lib/3_23.dart
@@ -1,33 +1,15 @@
 import 'dart:math';
 
 void main() {
-  print('주사위 : ${dice()}');
+  print('주사위 : ${dice(6)}');
 }
 
-int dice() {
+// 정수 num 하나를 받아서 정수 1 ~ num 까지를 반환한다.
+int dice(int num) {
   // 1~10 주사위
   var random = new Random();
-  return random.nextInt(10) + 1; //print(random.nextInt(6) + 1);를 삭제해줌
+  return random.nextInt(num) + 1; //print(random.nextInt(6) + 1);를 삭제해줌
 
 }
-int dice2() {
-  var random = new Random();
-  return random.nextInt(10) + 1;  //1~10주사위
-}
-int dice3() {
-  var random = new Random();
-  return random.nextInt(9) + 1; //1~9주사위
-}
-int dice4() {
-  var random = new Random();
-  return random.nextInt(8) + 1; //1~8주사위
-}
-int dice5() {
-  var random = new Random();
-  return random.nextInt(7) + 1; //1~7주사위
-}
-int dice6() {
-  var random = new Random();
-  return random.nextInt(6) + 1; //1~6주사위
-}
+
 
