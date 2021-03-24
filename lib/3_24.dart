006759bf4f7f2552ffcb09a7775a16417fe13577
Lss <tjdtn2323@gmail.com>
2021-03-24 11:39:04 +0900

    과학 성적 프로그램 추가3

--- lib/3_24.dart
+++ lib/3_24.dart
@@ -4,8 +4,9 @@ void main() {
   int kor = 100;
   int math = 50;
   int english = 70;
+  int science = 30;
 
-  int sum = total(kor, math, english);
+  int sum = total(kor, math, english, science);
   double average = sum / 3;
 
   print('합계 : $sum');
@@ -13,10 +14,10 @@ void main() {
   //.toStringAsFixed(2)소수점 두번째 자리에서 끊음.
 }
 
-int total(int korean, int math, int english) {
+int total(int korean, int math, int english, int science) {
   return korean + math + english;
 }
 
-double avg(int korean, int math, int english) {
-  return (korean + math + english) / 3;
+double avg(int korean, int math, int english, science) {
+  return (korean + math + english) / 4;
 }
