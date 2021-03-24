fe9a52b6e3173c7427e0c615e3efcc7a080de998
Lss <tjdtn2323@gmail.com>
2021-03-24 11:48:00 +0900

    List를 사용4

--- lib/3_24.dart
+++ lib/3_24.dart
@@ -1,13 +1,12 @@
 //List
 
 void main() {
-  int kor = 100;
-  int math = 50;
-  int english = 70;
-  int science = 30;
+  //int kor = 100; int math = 50;  int english = 70;  int science = 30;
+  //List를 사용함으로써 사용을 안하게 됨
+  List<int> scores = [100, 50, 70, 30];
 
-  int sum = total(kor, math, english, science);
-  double average = sum / 3;
+  int sum = total(scores[0], scores[1], scores[2], scores[3]);
+  double average = avg(scores[0], scores[1], scores[2], scores[3]);
 
   print('합계 : $sum');
   print('평균 : ${average.toStringAsFixed(2)}');
@@ -15,9 +14,9 @@ void main() {
 }
 
 int total(int korean, int math, int english, int science) {
-  return korean + math + english;
+  return korean + math + english + science;
 }
 
 double avg(int korean, int math, int english, science) {
-  return (korean + math + english) / 4;
+  return total(korean, math, english, science) / 4;
 }
