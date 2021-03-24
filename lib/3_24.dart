56d0b312eeca675b9ed3a1f1f4ea8c45196e40b7
Lss <tjdtn2323@gmail.com>
2021-03-24 12:09:54 +0900

    List를 활용한 계산법

--- lib/3_24.dart
+++ lib/3_24.dart
@@ -5,18 +5,24 @@ void main() {
   //List를 사용함으로써 사용을 안하게 됨
   List<int> scores = [100, 50, 70, 30];
 
-  int sum = total(scores[0], scores[1], scores[2], scores[3]);
-  double average = avg(scores[0], scores[1], scores[2], scores[3]);
+
+  int sum = total(scores);
+  double average = avg(scores);
 
   print('합계 : $sum');
   print('평균 : ${average.toStringAsFixed(2)}');
   //.toStringAsFixed(2)소수점 두번째 자리에서 끊음.
 }
 
-int total(int korean, int math, int english, int science) {
-  return korean + math + english + science;
+int total(List<int> scores) {
+  print(scores.length); // List의 갯수(scores의)
+  int result = 0;
+  for (int i = 0; i < scores.length; i++) {
+    result = result + scores[i];
+  }
+  return result;
 }
 
-double avg(int korean, int math, int english, science) {
-  return total(korean, math, english, science) / 4;
+double avg(List<int> scores) {
+  return total(scores) / scores.length;
 }
