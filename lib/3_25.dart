7b88499124c4d31860c681dc4099540c0ded00ce
Lss <tjdtn2323@gmail.com>
2021-03-25 13:36:53 +0900

    클래릭 숙제

--- lib/3_25.dart
+++ lib/3_25.dart
@@ -1,24 +1,52 @@
-
-
 void main() {
- Hero hero = Hero();    //인스턴스를 생성
- Hero hero2 = Hero();    //인스턴스를 생성
-
- hero.name = '용사';
- hero.hp = 100;
- hero.attack();
- hero2.name = '검사';
- hero2.hp = 100;
- hero2.attack();
-
- print(hero.name);
-
+  //Hero hero2 = Hero(); //인스턴스를 생성
+  //Hero hero = Hero(); //인스턴스를 생성
+  // hero.name = '용사';
+  // hero.hp = 100;
+  // hero.attack();
+//  hero2.name = '검사';
+  // hero2.hp = 100;
+//  hero2.attack();
+  // print(hero.name);
+  Cleric cleric = Cleric();
+  cleric.name = '성직자';
+  cleric.selfAid();
 }
+
 // Hero 타입을 새로 작성
 class Hero {
+  // field, global variable, 전역변수
   String name;
   int hp;
-  void attack(){
+
+  void attack() {
+    // local variable, 지역변수 hp
+    int hp = 100;
     print('공격!');
+  }
+}
+
+class Cleric {
+  String name;
+  int hp = 50;
+  int mp = 10;
+  final int maxHp = 50;
+  final int maxMp = 10;
+
+  void selfAid() {
+    print('이기적인 힐');
+    if (mp < 5) {
+      print('사용할수없습니다.');
+    } else {
+      mp = mp - 5;
+      hp = maxHp;
+
+      print('$hp');
+      print('$mp');
+    }
+  }
+
+  void pray() {
+
   }
 }
