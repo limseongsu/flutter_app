341712c52dc557878b1b640918c6d0c47a514735
limseongsu <79133729+limseongsu@users.noreply.github.com>
2021-03-30 10:25:15 +0900

    3_30Exam 11

---
+++ lib/3_30/3_30Exam1.dart
@@ -0,0 +1,22 @@
+// 정수 N을 입력받아 * 을 N 개만큼 출력하시오
+// 예1)
+// 4
+// ****
+//
+// 6
+// ******
+void main() {
+  Exam exam = Exam();
+  print(exam.solution(3));
+  print(exam.solution(6));
+  print(exam.solution(2));
+}
+class Exam {
+  String solution(int input) {
+    String result = '';
+    for (int i = 0; i < input; i++) {
+      result = result + '*';
+    }
+    return result;
+  }
+}
---
+++ lib/3_30/3_30Exam3.dart
@@ -0,0 +1,12 @@
+
+
+
+void main() {
+
+int a = 90;
+int b = 60;
+int triagle = 180 - (a + b);
+print(triagle);
+
+}
+
