4ed066e64b843121fe3f222d63e3977650c21364
limseongsu <79133729+limseongsu@users.noreply.github.com>
2021-03-30 10:56:27 +0900

    3_30Exam 12

--- lib/3_30/3_30Exam3.dart
+++ lib/3_30/3_30Exam3.dart
@@ -3,10 +3,13 @@
 
 void main() {
 
-int a = 90;
+
+  int a = 90;
 int b = 60;
 int triagle = 180 - (a + b);
 print(triagle);
 
 }
+
+
 
---
+++ lib/3_30Exam4.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_app/3_30/3_30.dart';
+
+void main() {
+  Exam exam = Exam();
+
+}
+
+class Exam{
+  int solution(int i) {
+    int result = 0;
+    result = 7 - i;
+    return result;
+  }
+}
