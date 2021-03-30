5fffd10a6c40981cdae273cf6579c5d854208b3b
limseongsu <79133729+limseongsu@users.noreply.github.com>
2021-03-30 18:25:26 +0900

    3_30Exam 20

---
+++ lib/3_30/3_30Exam12.dart
@@ -0,0 +1,21 @@
+void main() {
+
+  Exam exam = Exam();
+  print(exam.soultion(10, 3));
+  print(exam.soultion(100, 30));
+  print(exam.soultion(10, 30));
+
+
+}
+
+class Exam{
+  String soultion(int m, int n){
+
+
+  int a = m % n; //나머지
+  int b = m ~/ n; //몫
+  String result = '$b, $a';
+
+  return result;
+  }
+}
