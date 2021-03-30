f8029e99b787259393fe227db989f50dc3be529b
limseongsu <79133729+limseongsu@users.noreply.github.com>
2021-03-30 18:03:41 +0900

    3_30Exam 19

---
+++ lib/3_30/3_30Exam11.dart
@@ -0,0 +1,37 @@
+
+
+
+void main() {
+
+  Exam exam = Exam();
+  print(exam.solution(-10));
+  print(exam.solution(10));
+}
+
+class Exam{
+
+  int solution(int a) {
+    int result = 0;
+    if (a < 0){
+      result = a * -1;
+    } else if (a > 0) {
+      result = a;
+    }
+    return result;
+  }
+}
+
+
+
+// class Exam {
+//   int solution(int number) {
+//     int answer = 0;
+//     for (int i = 0; i < 10; i ++)
+//       if (number < 0) {
+//         answer = number * -1;
+//       } else if (number > 0){
+//         answer = number;
+//       }
+//     return answer;
+//   }
+// }
