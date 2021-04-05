62ff2f4433e6b8f43946a50352a7503d45c0f97f
limseongsu <79133729+limseongsu@users.noreply.github.com>
2021-04-05 16:59:36 +0900

    4_5 위젯만들기 2

--- lib/4_5/Page115.dart
+++ lib/4_5/Page115.dart
@@ -14,31 +14,33 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHeomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> {
   var _text = 'Hello';
-}
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: Text('Hello World'),
-    ),
-    body: Text (
-      _text,
-      style: TextStyle(fontSize: 40),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        setState(() {
-          _text = 'World';
-        });
-      },
-      child: Icon(Icons.touch_app),
-    ),
-  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+      ),
+      body: Text(
+        _text,
+        style: TextStyle(fontSize: 40),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _text = 'World';
+          });
+        },
+        child: Icon(Icons.touch_app),
+      ),
+    );
+  }
 }
